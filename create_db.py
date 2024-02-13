b22b101b18cb37f2f75cceb69974a6803f402bdd
Estacio Lima <estaciocerqueira7@gmail.com>
2024-02-13 12:35:37 -0300

    data cleaning on-going

--- create_db.py
+++ create_db.py
@@ -1,26 +1,25 @@
 import pandas as pd
 from sqlalchemy import create_engine, inspect, text
-#import mysql.connector
+import mysql.connector
 
-df = pd.read_csv('example.csv')
+df = pd.read_csv('datasets/database-1701392111.8423762.csv')
+df.columns = [c.strip() for c in df.columns]
 
 engine = create_engine("{dialect}+{driver}://{username}:{password}@{host}/{database}".format(
     dialect='mysql',
     driver='mysqlconnector',
     username='root',
-    password='admin',
+    password='password',
     host='localhost',
-    database='carros_usados'))
-
-df.to_sql('carros', engine, index=False)
+    database='mysql'))
 
 with engine.connect() as connection:
     connection.execute(text('CREATE DATABASE IF NOT EXISTS carros_usados;'))
     connection.execute(text('USE carros_usados;'))
     connection.commit()
-
+                                                                                                                                                                                                                
+df.to_sql('carros', engine, index=False)
     
-
 inspector = inspect(engine)
 
 print(inspector.get_table_names('carros_usados'))
--- helpers.py
+++ helpers.py
@@ -1,4 +1,12 @@
+import unidecode
+
 def collect_car_pages(bs):
+    '''
+    This function returns a list 'car_pages' with the links for the car pages to be used as argument to the car_crawler() function later on.
+    
+    Arguments: 
+        - bs -> BeutifulSoup object
+    '''
     pages_links = bs.findAll('div', {'class':'active mui-style-14y9di7'})
     car_pages = []
 
@@ -7,3 +15,16 @@ def collect_car_pages(bs):
         car_pages.append(car_page)
 
     return car_pages
+
+
+def convert_valor_to_numerical(valor):
+    valor = unidecode.unidecode(valor) # convert '\xa' to ASCII
+    valor = valor.replace('.','') # remove '.' value separator from string
+    valor = valor.split()
+
+    if len(valor) == 1:
+        return int(valor[0])
+    elif len(valor) == 2:
+        return int(valor[1])
+    else:
+        raise Exception('More fields than expected: {}'.format(valor))
